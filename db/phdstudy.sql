31cbeb0fe7243bfa57e85db8634b9d26ad716aa8
pepe <pepe@4f264560-c2f2-0310-a931-971050087e38>
2005-04-11 08:22:20 +0000

    Changed development to sqlite
    git-svn-id: svn://localhost/phdstudy/trunk@23 4f264560-c2f2-0310-a931-971050087e38

---
+++ db/phdstudy.sql
@@ -0,0 +1,119 @@
+
+--
+-- Table structure for table candidates
+--
+
+DROP TABLE candidates;
+CREATE TABLE candidates (
+  id integer primary key,
+  name varchar(50),
+  last_name varchar(50),
+  title varchar(20),
+  coridor_id integer,
+  study_end date,
+  university varchar(100),
+  birth_on date,
+  birth_number varchar(11),
+  birth_at varchar(100),
+  email varchar(100),
+  street varchar(100),
+  city varchar(100),
+  zip varchar(10),
+  postal_street varchar(100),
+  phone varchar(50),
+  state varchar(50),
+  studied_branch varchar(200),
+  employer varchar(50),
+  employer_address varchar(50),
+  employer_email varchar(50),
+  employer_phone varchar(50),
+  position varchar(50),
+  department_id integer,
+  language1 integer,
+  language2 integer,
+  study_id integer,
+  postal_city varchar(100),
+  postal_zip varchar(10),
+  faculty varchar(100),
+  studied_specialization varchar(100),
+  study_theme varchar(100),
+  finished_on timestamp,
+  note text,
+  number integer,
+  postal_number integer
+);
+
+--
+-- Table structure for table coridors
+--
+
+DROP TABLE coridors;
+CREATE TABLE coridors (
+  id integer primary key,
+  name varchar(50),
+  faculty_id varchar(50)
+);
+
+--
+-- Table structure for table departments
+--
+
+DROP TABLE departments;
+CREATE TABLE departments (
+  id integer primary key,
+  name varchar(50),
+  faculty_id integer
+);
+
+--
+-- Table structure for table faculties
+--
+
+DROP TABLE faculties;
+CREATE TABLE faculties (
+  id integer primary key,
+  name varchar(50)
+);
+
+--
+-- Table structure for table languages
+--
+
+DROP TABLE languages;
+CREATE TABLE languages (
+  id integer primary key,
+  name varchar(50)
+);
+
+--
+-- Table structure for table sessions
+--
+
+DROP TABLE sessions;
+CREATE TABLE sessions (
+  id integer primary key,
+  sessid text,
+  data text
+);
+
+--
+-- Table structure for table studies
+--
+
+DROP TABLE studies;
+CREATE TABLE studies (
+  id integer primary key,
+  name varchar(50)
+);
+
+--
+-- Table structure for table users
+--
+
+DROP TABLE users;
+CREATE TABLE users (
+  id integer primary key,
+  login varchar(80),
+  password varchar(40)
+);
+
--- db/phdstudy_development.sql
+++ db/phdstudy_development.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.9
 --
--- Host: localhost    Database: phdstudy_development
+-- Host: gravastar.cz    Database: phdstudy_development
 -- ------------------------------------------------------
--- Server version	4.1.9-log
+-- Server version	4.1.10
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,12 +16,44 @@
 -- Table structure for table `candidates`
 --
 
+DROP TABLE IF EXISTS `candidates`;
 CREATE TABLE `candidates` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(50) default NULL,
   `last_name` varchar(50) default NULL,
+  `title` varchar(20) default NULL,
   `coridor_id` int(11) default NULL,
-  `study_average` float default NULL,
+  `study_end` date default NULL,
+  `university` varchar(100) default NULL,
+  `birth_on` date default NULL,
+  `birth_number` varchar(11) default NULL,
+  `birth_at` varchar(100) default NULL,
+  `email` varchar(100) default NULL,
+  `street` varchar(100) default NULL,
+  `city` varchar(100) default NULL,
+  `zip` varchar(10) default NULL,
+  `postal_street` varchar(100) default NULL,
+  `phone` varchar(50) default NULL,
+  `state` varchar(50) default NULL,
+  `studied_branch` varchar(200) default NULL,
+  `employer` varchar(50) default NULL,
+  `employer_address` varchar(50) default NULL,
+  `employer_email` varchar(50) default NULL,
+  `employer_phone` varchar(50) default NULL,
+  `position` varchar(50) default NULL,
+  `department_id` int(11) default NULL,
+  `language1` int(1) default NULL,
+  `language2` int(1) default NULL,
+  `study_id` int(11) default NULL,
+  `postal_city` varchar(100) default NULL,
+  `postal_zip` varchar(10) default NULL,
+  `faculty` varchar(100) default NULL,
+  `studied_specialization` varchar(100) default NULL,
+  `study_theme` varchar(100) default NULL,
+  `finished_on` timestamp NULL default NULL,
+  `note` text,
+  `number` int(4) default NULL,
+  `postal_number` int(4) default NULL,
   PRIMARY KEY  (`id`),
   KEY `name` (`name`),
   KEY `last_name` (`last_name`)
@@ -31,6 +63,7 @@ CREATE TABLE `candidates` (
 -- Table structure for table `coridors`
 --
 
+DROP TABLE IF EXISTS `coridors`;
 CREATE TABLE `coridors` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(50) default NULL,
@@ -40,9 +73,22 @@ CREATE TABLE `coridors` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
+-- Table structure for table `departments`
+--
+
+DROP TABLE IF EXISTS `departments`;
+CREATE TABLE `departments` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(50) default NULL,
+  `faculty_id` int(11) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
 -- Table structure for table `faculties`
 --
 
+DROP TABLE IF EXISTS `faculties`;
 CREATE TABLE `faculties` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(50) default NULL,
@@ -51,9 +97,44 @@ CREATE TABLE `faculties` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
+-- Table structure for table `languages`
+--
+
+DROP TABLE IF EXISTS `languages`;
+CREATE TABLE `languages` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(50) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `sessions`
+--
+
+DROP TABLE IF EXISTS `sessions`;
+CREATE TABLE `sessions` (
+  `id` int(11) NOT NULL auto_increment,
+  `sessid` text,
+  `data` text,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `studies`
+--
+
+DROP TABLE IF EXISTS `studies`;
+CREATE TABLE `studies` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(50) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL auto_increment,
   `login` varchar(80) character set latin1 collate latin1_bin default NULL,
