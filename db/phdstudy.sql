e281f258f947b0b06f9e8e94c9d81690e288b301
pepe <pepe@4f264560-c2f2-0310-a931-971050087e38>
2005-04-25 09:06:14 +0000

    db/phdstudy.sql: redone with new application structure
    git-svn-id: svn://localhost/phdstudy/trunk@37 4f264560-c2f2-0310-a931-971050087e38

--- db/phdstudy.sql
+++ db/phdstudy.sql
@@ -82,6 +82,7 @@ CREATE TABLE faculties (
 
 --
 -- Table structure for table languages
+-- just stub for admit form
 --
 
 DROP TABLE languages;
@@ -138,20 +139,14 @@ CREATE TABLE people (
 DROP TABLE indexes;
 CREATE TABLE indexes (
   id integer primary key,
-  year integer,
+  -- year integer, -- should be computed in class?
   study_id integer,
   student_id integer,
-  tutorship_id integer
-);
-
---
--- Table structure for table study plans statuses
---
-
-DROP TABLE study_plans_statuses;
-CREATE TABLE study_plans_statuses (
-  id integer primary key,
-  name varchar(100)
+  department_id integer,
+  coridor_id integer,
+  tutorship_id integer,
+  created_on timestamp,
+  updated_on timestamp
 );
 
 --
@@ -163,8 +158,170 @@ CREATE TABLE study_plans (
   id integer primary key,
   index_id integer,
   actual integer,
-  status_id integer
+  created_on timestamp,
+  updated_on timestamp,
+  admited_on timestamp,
+  canceled_on timestamp,
+  approved_on timestamp
+  -- status_id integer -- deprecated in favor of method drven checks
 );
+
+--
+-- Table structure for table plansubjects
+--
+
+DROP TABLE plan_subjects;
+CREATE TABLE plan_subjects (
+  id integer primary key,
+  study_plan_id integer,
+  subject_id integer,
+  created_on timestamp,
+  updated_on timestamp
+);
+
+--
+-- Table structure for table exam
+--
+
+DROP TABLE exam;
+CREATE TABLE exam (
+  id integer primary key,
+  index_id integer,
+  first_examinator_id integer,
+  second_examinator_id integer,
+  result integer,
+  questions varchar(100),
+  subject_id integer,
+  created_on timestamp,
+  updated_on timestamp
+);
+
+--
+-- Table structure for table interupts
+--
+
+DROP TABLE interupts;
+CREATE TABLE interupts (
+  id integer primary key,
+  study_plan_id integer,
+  note varchar(100),
+  created_on timestamp,
+  updated_on timestamp
+);
+
+--
+-- Table structure for table approvments
+-- aproovements are related to study_plans, disert_themes and interupts
+--
+
+DROP TABLE approvements;
+CREATE TABLE approvements (
+  id integer primary key,
+  type varchar(30),
+  document_id integer,
+  tutor_deliverance_id integer,
+  leader_deliverance_id integer,
+  dean_deliverance_id integer,
+  board_deliverance_id integer,
+  created_on timestamp,
+  updated_on timestamp
+);
+
+--
+-- Table structure for table deliverance
+--
+
+DROP TABLE deliverance;
+CREATE TABLE deliverance (
+  id integer primary key,
+  note varchar(100),
+  result integer,
+  created_on timestamp,
+  updated_on timestamp
+);
+
+
+
+--
+-- Table structure for table tutorships
+--
+
+DROP TABLE tutorships;
+CREATE TABLE tutorships (
+  id integer primary key,
+  department_id integer,
+  employee_id integer,
+  coridor_id integer,
+  created_on timestamp,
+  updated_on timestamp
+);
+
+--
+-- Table structure for table leaderships
+--
+
+DROP TABLE leaderships;
+CREATE TABLE leaderships (
+  id integer primary key,
+  department_id integer,
+  employee_id integer,
+  created_on timestamp,
+  updated_on timestamp
+);
+
+--
+-- Table structure for table deanships
+--
+
+DROP TABLE deanships;
+CREATE TABLE deanships (
+  id integer primary key,
+  faculty_id integer,
+  employee_id integer,
+  created_on timestamp,
+  updated_on timestamp
+);
+
+
+--
+-- Table structure for table disert_themes
+--
+
+DROP TABLE disert_themes;
+CREATE TABLE disert_themes (
+  id integer primary key,
+  title varchar(100),
+  index_id integer,
+  methodology_file varchar(30),
+  created_on timestamp,
+  updated_on timestamp
+);
+
+
+-- deprecated in favor of simple methodology file in disert
+-- Table structure for table methodologies
+--
+
+-- DROP TABLE methodologies;
+-- CREATE TABLE methodologies (
+--   id integer primary key,
+--   text varchar(255),
+--   disert_theme_id integer
+-- );
+
+-- deprecated in favor of method driven checks
+-- Table structure for table study plans statuses
+--
+
+-- DROP TABLE study_plans_statuses;
+-- CREATE TABLE study_plans_statuses (
+  -- id integer primary key,
+  -- name varchar(100)
+-- );
+
+-----------------------------------------
+-- folowing structures are related to DWH
+-----------------------------------------
 
 --
 -- Table structure for table addresses
@@ -216,17 +373,6 @@ CREATE TABLE contact_types (
   name varchar(20)
 );
 
---
--- Table structure for table tutorships
---
-
-DROP TABLE tutorships;
-CREATE TABLE tutorships (
-  id integer primary key,
-  department_id integer,
-  tutor_id integer,
-  coridor_id integer
-);
 
 --
 -- Table structure for table titles
@@ -239,29 +385,6 @@ CREATE TABLE titles (
   before integer
 );
 
-
---
--- Table structure for table disert_themes
---
-
-DROP TABLE disert_themes;
-CREATE TABLE disert_themes (
-  id integer primary key,
-  title varchar(100),
-  index_id integer
-);
-
-
---
--- Table structure for table methodologies
---
-
-DROP TABLE methodologies;
-CREATE TABLE methodologies (
-  id integer primary key,
-  text varchar(255),
-  disert_theme_id integer
-);
 
 --
 -- Table structure for table sessions
