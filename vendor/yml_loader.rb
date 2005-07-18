981cc74dd5d25cff64b262fed0021f8461075512
pepe <pepe@4f264560-c2f2-0310-a931-971050087e38>
2005-07-18 13:39:48 +0000

    config/database.yml: changed to normal
    git-svn-id: svn://localhost/phdstudy/trunk@145 4f264560-c2f2-0310-a931-971050087e38

--- vendor/yml_loader.rb
+++ vendor/yml_loader.rb
@@ -1,12 +1,16 @@
 # loads all yml files in dumps directory 
 class YMLLoader
   # provide directory to function and it reads and create all objects 
-  def self.load(dir)
+  def self.load(dir = 'dumps')
     Dir.open(dir).each do |file|
       if file =~ /yml/
         File.open("#{dir}/#{file}", 'r') {|out| YAML.load(out)}.each {|obj| obj.create}
       end
     end
+  end
+  # creates dumps from classes passed in array to function  
+  # TODO
+  def self.dump(array, dir = 'dumps')
   end
 end
 
