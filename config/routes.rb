533245daa95790fcd68289e42f923b324e39f562
pepe <pepe@4f264560-c2f2-0310-a931-971050087e38>
2006-01-31 12:25:05 +0000

    Fixed creating study plan by other, also logging without study plan for student. Fixed route to error_url.
    git-svn-id: svn://localhost/phdstudy/trunk@427 4f264560-c2f2-0310-a931-971050087e38

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,7 @@ ActionController::Routing::Routes.draw do |map|
   # add path for prijimacky
   map.connect 'prijimacky/:action/:id', :controller => 'form'
   # error url
-  map.error 'account/error'
+  map.error 'account/error', :controller => 'account', :action => 'error'
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id'
 end
