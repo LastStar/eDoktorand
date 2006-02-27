58a6b46ac5d6c230cc6cdcb4a0cdefe773f89be2
pepe <pepe@4f264560-c2f2-0310-a931-971050087e38>
2006-02-27 12:36:30 +0000

    Fixed examinator bug in creating probation term
    git-svn-id: svn://localhost/phdstudy/trunk@501 4f264560-c2f2-0310-a931-971050087e38

--- app/models/examinator.rb
+++ app/models/examinator.rb
@@ -1,11 +1,16 @@
 class Examinator < Person
   has_one :department_employment, :foreign_key => 'person_id'
-  def self.for_html_select(user)
-    if user.has_role?('faculty_secretary')
+  def self.for_html_select(user, options = {})
+    result = if user.has_role?('faculty_secretary')
       find_for_faculty(user.person.faculty.id)
     elsif user.has_one_of_roles?(['department_secretary', 'tutor', 'leader'])
       find_for_department(user.person.department.id)
     end.map {|e| [e.display_name, e.id]}
+    if options[:include_null]
+      [['---', 0]].concat(result)
+    else
+      result
+    end
   end
 
   def self.find_for_department(department)
