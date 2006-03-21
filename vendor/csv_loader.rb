cd0951b886007ea6331750eabc993f1ed1dfef37
pepe <pepe@4f264560-c2f2-0310-a931-971050087e38>
2006-03-21 11:02:42 +0000

    Added dump of KJ examinators and fixed bug in csv loader.
    git-svn-id: svn://localhost/phdstudy/trunk@525 4f264560-c2f2-0310-a931-971050087e38

--- vendor/csv_loader.rb
+++ vendor/csv_loader.rb
@@ -437,11 +437,13 @@ include Log4r
     @@mylog.info "Loading examinators..."
     CSV::Reader.parse(File.open(file, 'rb'), ';') do |row|
       unless p = Examinator.find_by_uic(row[0])
-        p = Examinator.new('firstname' => row[1], 'lastname' => row[2])
+        p = Examinator.new('firstname' => row[2], 'lastname' => row[1])
+        p.uic = row[0]
         p.title_before = Title.find(row[3]) unless row[3].empty?
         p.title_after = Title.find(row[4]) unless row[4].empty?
       end
       p.department_employment = DepartmentEmployment.create('unit_id' => row[5])
+      @@mylog.info "Saving #{p.type} #{p.lastname}"
       p.save
     end
   end
