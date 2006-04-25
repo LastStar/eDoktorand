25a58ee28fe68d2b382d83d2498d5237254cd224
wolfi <wolfi@4f264560-c2f2-0310-a931-971050087e38>
2006-04-25 14:48:29 +0000

    translated exam_term.rb to CZ and some other things
    git-svn-id: svn://localhost/phdstudy/trunk@564 4f264560-c2f2-0310-a931-971050087e38

--- app/models/exam_term.rb
+++ app/models/exam_term.rb
@@ -1,12 +1,12 @@
 class ExamTerm < ActiveRecord::Base
 	belongs_to :coridor
 	belongs_to :chairman, :class_name => 'Tutor', :foreign_key => 'chairman_id'
-	validates_presence_of :coridor_id, :message => 'Termín musí mít přidělený koridor'
-	validates_presence_of :room, :message => 'Termín musí mít přidělenou místnost'
-	validates_presence_of :chairman_id, :message => 'Termín musí mít přiděleného předsedu'
-	validates_presence_of :start_time, :message => 'Termín musí mít přidělený čas začátku'
-	validates_presence_of :date, :message => 'Termín musí mít přidělený datum začátku'
-	validates_presence_of :first_examinator, :message => 'Termín musí mít přiděleného prvního zkoušejícího'
-	validates_presence_of :second_examinator, :message => 'Termín musí mít přiděleného druhého zkoušejícího'
+	validates_presence_of :coridor_id, :message => _("Term must has assigned corridor")
+	validates_presence_of :room, :message => _("Term must has assigned room")
+	validates_presence_of :chairman_id, :message => _("Term must has assigned chairman")
+	validates_presence_of :start_time, :message => _("Term must has assigned time of begining")
+	validates_presence_of :date, :message => _("Term must has assigned date of begining")
+	validates_presence_of :first_examinator, :message => _("Term must has assigned first examiner")
+	validates_presence_of :second_examinator, :message => _("Term must has assigned second examiner")
 #	validates_uniqueness_of :coridor_id, :message => 'Pro tento koridor termín již existuje'
 end
