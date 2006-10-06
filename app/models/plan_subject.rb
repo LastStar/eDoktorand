4f27180ee4d67e892ec3dca285204ad79b4d4bad
pepe <pepe@4f264560-c2f2-0310-a931-971050087e38>
2006-10-06 08:13:50 +0000

    - added sorting of students for exam selecting
    git-svn-id: svn://localhost/phdstudy/trunk@761 4f264560-c2f2-0310-a931-971050087e38

--- app/models/plan_subject.rb
+++ app/models/plan_subject.rb
@@ -51,7 +51,9 @@ class PlanSubject < ActiveRecord::Base
     plan_subjects = find(:all, :include => [{:study_plan => :index}],
                          :conditions => [sql, subject_id])
     if options[:students]
-      plan_subjects.map {|ps| ps.study_plan.index.student}
+      plan_subjects.map {|ps| ps.study_plan.index.student}.sort do |x,y|
+        x.display_name <=> y.display_name
+      end
     else
       plan_subjects
     end
