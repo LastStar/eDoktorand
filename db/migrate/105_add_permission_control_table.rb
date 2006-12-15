49b203fa73029b091d5c2102bdf834d53cb85da1
wolfi <wolfi@4f264560-c2f2-0310-a931-971050087e38>
2006-12-15 14:21:46 +0000

    added control table to scholarships for faculty and department secretary
    git-svn-id: svn://localhost/phdstudy/trunk@924 4f264560-c2f2-0310-a931-971050087e38

---
+++ db/migrate/105_add_permission_control_table.rb
@@ -0,0 +1,12 @@
+class AddPermissionControlTable < ActiveRecord::Migration
+  def self.up
+    Role.find(2).permissions <<
+      Permission.create(:name => 'scholarships/control_table')
+    Role.find(7).permissions <<
+      Permission.(:name => 'scholarships/control_table')
+  end
+
+  def self.down
+    Permission.find_by_name('scholarships/control_table').destroy
+  end
+end
