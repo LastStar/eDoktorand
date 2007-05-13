000a415ccc32a288074165e3c1fac16258c24ef0
pepe <pepe@4f264560-c2f2-0310-a931-971050087e38>
2007-05-13 11:24:03 +0000

    - created easy diploma supplement management
    git-svn-id: svn://localhost/phdstudy/trunk@1045 4f264560-c2f2-0310-a931-971050087e38

---
+++ test/unit/diploma_supplement_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class DiplomaSupplementTest < Test::Unit::TestCase
+  fixtures :diploma_supplements
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
