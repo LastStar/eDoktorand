4a8a47c6a8f7c2d905d9ce5551d47f71f0327ac5
pepe <pepe@4f264560-c2f2-0310-a931-971050087e38>
2007-08-28 19:43:39 +0000

    fixed migration
    git-svn-id: svn://localhost/phdstudy/trunk@1125 4f264560-c2f2-0310-a931-971050087e38

--- db/migrate/135_enlarge_address_city_and_state.rb
+++ db/migrate/135_enlarge_address_city_and_state.rb
@@ -1,11 +1,11 @@
 class EnlargeAddressCityAndState < ActiveRecord::Migration
   def self.up
-    change_column :addresses, :city, :limit => 100
-    change_column :addresses, :state, :limit => 100
+    change_column :addresses, :city, :string, :limit => 100
+    change_column :addresses, :state, :string, :limit => 100
   end
 
   def self.down
-    change_column :addresses, :city, :limit => 20
-    change_column :addresses, :state, :limit => 20
+    change_column :addresses, :city, :string, :limit => 20
+    change_column :addresses, :state, :string, :limit => 20
   end
 end
