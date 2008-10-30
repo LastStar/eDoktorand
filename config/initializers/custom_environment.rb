37a0461a89bde65eb6af8101b12087458a3e76ab
Josef Pospíšil <pepe@gravastar.cz>
2008-10-30 15:56:50 +0100

    repaired exception notification

--- config/initializers/custom_environment.rb
+++ config/initializers/custom_environment.rb
@@ -7,7 +7,7 @@ require 'jcode'
 
 # mixins
 require 'first_char_changer'
-ExceptionNotifier.exception_recipients = %w(pepe@gravastar.cz dvorakv@oikt.czu.cz)
+ExceptionNotifier.exception_recipients = %w(pepe@gravastar.cz masak@gravastar.cz)
 ExceptionNotifier.sender_address = 
   %("Edoktorand Exception Notifier" <exception.notifier@edoktorand.czu.cz>)
 require 'gettext/rails'
