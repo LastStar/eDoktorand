4ddf5c6f292bc305a56bf7b3ef6da1a16ac5c188
Josef Pospíšil <pepe@gravastar.cz>
2009-02-03 11:42:55 +0100

    added translate plugin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  Translate::Routes.translation_ui(map) if RAILS_ENV != "production"
 
   # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
