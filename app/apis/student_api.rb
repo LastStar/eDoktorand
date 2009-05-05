7c52f2f5944f0d3767c2cc178cab8995fffdb036
Josef Pospíšil <pepe@gravastar.cz>
2009-05-05 14:59:26 +0200

    removed create services from student api

--- app/apis/student_api.rb
+++ app/apis/student_api.rb
@@ -55,14 +55,6 @@ class StudentApi < ActionWebService::API::Base
              :expects => [IndexHash],
              :returns => [:string]
 
-  api_method :create_student,
-             :expects => [StudentHash],
-             :returns => [:string]
-
-  api_method :create_index,
-             :expects => [IndexHash],
-             :returns => [:string]
-
   # do not implement here
   api_method :get_uic_by_birth_num,
              :expects => [:string],
