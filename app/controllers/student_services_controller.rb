c00b039a80792e0ec0da26780a026d6093524dcf
Martin Rychlovsky <masak@BigMonster.local>
2009-06-04 14:29:25 +0200

    Added method for webservices for update or create user account for student

--- app/controllers/student_services_controller.rb
+++ app/controllers/student_services_controller.rb
@@ -44,38 +44,3 @@ web_service_scaffold :invoke
     end
     return uic_accounts
   end
-
-  # service which returns student detail by uic
-  def find_student_by_uic(uic)
-    return Student.find_by_uic(uic).to_service_struct
-  end
-
-  # returns student's index detail
-  def find_index_for_student_uic(uic)
-    return Student.find_by_uic(uic).index.to_service_struct
-  end
-
-  # updates student with student hash content
-  def update_student_with_uic(student_hash)
-    student = Student.find_by_uic(student_hash.uic)
-    return student.update_with_hash(student_hash)
-  end
-
-  # updates index with index hash content
-  def update_index_with_uic(index_hash)
-    index = Student.find_by_uic(index_hash.uic).index
-    return index.update_with_hash(index_hash)
-  end
-
-  # creates student from student hash content
-  def create_student(student_hash)
-    return 'success'
-  end
-
-  # creates index from index hash content
-  def create_index(index_hash)
-    return 'success'
-  end
-
-
-end
