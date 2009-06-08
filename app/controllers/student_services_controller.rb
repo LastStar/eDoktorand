692c681f65311766efcfdd9346de423f74bc8767
Martin Rychlovsky <masak@BigMonster.local>
2009-06-08 14:19:20 +0200

    Added service for get account number of student from uic

--- app/controllers/student_services_controller.rb
+++ app/controllers/student_services_controller.rb
@@ -28,6 +28,33 @@ web_service_scaffold :invoke
     return account
   end
 
+  # service which returns student detail by uic
+  def find_student_by_uic(uic)
+    return Student.find_by_uic(uic).to_service_struct
+  end
+
+  # returns student's index detail
+  def find_index_for_student_uic(uic)
+    return Student.find_by_uic(uic).index.to_service_struct
+  end
+
+  # updates student with student hash content
+  def update_student_with_uic(student_hash)
+    student = Student.find_by_uic(student_hash.uic)
+    return student.update_with_hash(student_hash)
+  end
+
+  # updates index with index hash content
+  def update_index_with_uic(index_hash)
+    index = Student.find_by_uic(index_hash.uic).index
+    return index.update_with_hash(index_hash)
+  end
+
+  # creates student from student hash content
+  def create_student(student_hash)
+    return 'success'
+  end
+
   #return array of uic and bank account
   def get_account_uic_array
     uic_accounts = []
