b4d58c528508a8c045f745d62d081efdde0958d9
Jan Kricka <krica@gravastar.cz>
2009-07-22 14:20:21 +0200

    translations + time form

--- app/helpers/interupts_helper.rb
+++ app/helpers/interupts_helper.rb
@@ -3,11 +3,14 @@ module InteruptsHelper
   # TODO remove all magic numbers 
   # prints select for interupt start date
   def start_date_select
+    
     date_select(:interupt, :start_on,
                 :start_year => Date.today.year - 4,
                 :end_year => Date.today.year + 1,
                 :use_month_numbers => true,
-                :discard_day => true)
+                :discard_day => true,
+                :order => [:year, :month, :day]
+                )
 
   end
 end
