1f1d168d106c1372fd6c7db9e1f080cfce3a893e
Jan Stibal <stibi@gravastar.cz>
2009-09-18 11:44:06 +0200

    added permissions to secretary department to emails

--- db/migrate/20090918092701_add_permissions_to_secretary_department_to_email.rb
+++ db/migrate/20090918092701_add_permissions_to_secretary_department_to_email.rb
@@ -1,20 +1,19 @@
-class AddPermissionToTutorForEmails < ActiveRecord::Migration
+class AddPermissionsToSecretaryDepartmentToEmail < ActiveRecord::Migration
   def self.up
-    Role.find(4).permissions <<
+    Role.find(7).permissions <<
     Permission.find_by_name('students/mail_list')
-    Role.find(4).permissions <<
+    Role.find(7).permissions <<
     Permission.find_by_name('students/admin_edit_mail')
-    Role.find(4).permissions <<
+    Role.find(7).permissions <<
     Permission.find_by_name('students/admin_update_mail')
   end
 
   def self.down
     permission = Permission.find_by_name('students/mail_list')
-    Role.find(4).permissions.delete(permission)
+    Role.find(7).permissions.delete(permission)
     permission = Permission.find_by_name('students/admin_edit_mail')
-    Role.find(4).permissions.delete(permission)
+    Role.find(7).permissions.delete(permission)
     permission = Permission.find_by_name('students/admin_update_mail')
-    Role.find(4).permissions.delete(permission)
+    Role.find(7).permissions.delete(permission)
   end
 end
-
