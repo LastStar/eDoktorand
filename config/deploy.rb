c4d226f231cc997c90cdd1c185db8ffa72a4042b
Josef Pospíšil <pepe@gravastar.cz>
2009-11-02 13:32:04 +0100

    Changed deploy path on production server

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ deploy.application = "edoktorand.czu.cz"
 deploy.repository = 'ssh://git@gravastar.cz:2223/git/phdstudy'
 deploy.user = 'www'
 deploy.hosts = ['edoktorand']
-deploy.path = '/usr/www/test'
+deploy.path = '/usr/www/apps'
 deploy.branch = 'deploy'
 deploy.ssh_opts = '-p 2222'
 
