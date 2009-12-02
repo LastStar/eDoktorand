f111b0dff5e195eb40000182a9f2cfaaff1664d4
Josef Pospíšil <pepe@gravastar.cz>
2009-12-02 14:30:35 +0100

    Added saving of enroled student to candidate

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -227,7 +227,8 @@ class Candidate < ActiveRecord::Base
     create_postal_address(student.id) if self.postal_city
     student.email = self.email
     student.phone = self.phone if self.phone
-    student.save
+    
+    self.update_attribute(:student_id, student.id) if student.save
     return student
   end
 
