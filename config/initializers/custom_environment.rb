888c99916b1b7a7f573162978d6f230a9de6ca2c
Josef Pospíšil <pepe@gravastar.cz>
2010-01-11 15:28:51 +0100

    Removed masak from exception notifier - http://www.pivotaltracker.com/story/show/2153565

--- config/initializers/custom_environment.rb
+++ config/initializers/custom_environment.rb
@@ -7,7 +7,7 @@ require 'jcode'
 
 # mixins
 require 'first_char_changer'
-ExceptionNotifier.exception_recipients = %w(pepe@gravastar.cz masak@gravastar.cz)
+ExceptionNotifier.exception_recipients = %w(pepe@gravastar.cz)
 ExceptionNotifier.sender_address = 
   %("Edoktorand Exception Notifier" <exception.notifier@edoktorand.czu.cz>)
 
