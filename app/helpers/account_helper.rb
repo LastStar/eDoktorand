9ade722b50b1301ca87d07582b76ee1d53e37227
Jiří Beneš <benes@nandu.cz>
2010-01-18 22:02:00 +0100

    Restyled homepage; Restyled students list;

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -1,7 +1,7 @@
 module AccountHelper
   def locale_link
     if I18n.locale != 'cs'
-      link_to image_tag('cz.png'), locale_url(:lang => 'cs')
+      link_to image_tag('cz.png', :style => 'margin: 5px 0px 7px 2px'), locale_url(:lang => 'cs')
     else
       link_to image_tag('gb.png'), locale_url(:lang => 'en')
     end
