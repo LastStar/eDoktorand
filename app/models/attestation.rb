a8eb346e0b00c46b040120ceb2bdb4b16cfbb7f0
Josef Pospíšil <pepe@gravastar.cz>
2010-03-22 12:06:33 +0100

    Added first specification to atestation

--- app/models/attestation.rb
+++ app/models/attestation.rb
@@ -3,30 +3,32 @@ class Attestation < Approval
   has_one :attestation_detail
   belongs_to :study_plan, :foreign_key => 'document_id'
 
-  # returns date of actual attestation on faculty
-  def self.next_for_faculty(faculty)
-    faculty = faculty.id if faculty.is_a? Faculty
-    year = Date.today.year
-    if Date.today > (date = Date.civil(year, FACULTY_CFG[faculty]['attestation_month'], 
-    FACULTY_CFG[faculty]['attestation_day']))
-      year += 1
-      return Date.civil(year, FACULTY_CFG[faculty]['attestation_month'], 
-      FACULTY_CFG[faculty]['attestation_day'])
+  class << self
+    # returns date of actual atestation on faculty
+    def next_for_faculty(faculty)
+      faculty = faculty.id if faculty.is_a? Faculty
+      year = Date.today.year
+      if Date.today > (date = Date.civil(year, FACULTY_CFG[faculty]['atestation_month'], 
+      FACULTY_CFG[faculty]['atestation_day']))
+        year += 1
+        return Date.civil(year, FACULTY_CFG[faculty]['atestation_month'], 
+        FACULTY_CFG[faculty]['atestation_day'])
+      end
+      return date
     end
-    return date
-  end
 
-  # returns date of actual attestation on faculty
-  def self.actual_for_faculty(faculty)
-    faculty = faculty.id if faculty.is_a? Faculty
-    year = Date.today.year
-    if Date.today <= (date = Date.civil(year, FACULTY_CFG[faculty]['attestation_month'], 
-    FACULTY_CFG[faculty]['attestation_day']))
-      year -= 1
-      return Date.civil(year, FACULTY_CFG[faculty]['attestation_month'], 
-      FACULTY_CFG[faculty]['attestation_day'])
+    # returns date of actual atestation on faculty
+    def actual_for_faculty(faculty)
+      faculty = faculty.id if faculty.is_a? Faculty
+      year = Date.today.year
+      if Date.today <= (date = Date.civil(year, FACULTY_CFG[faculty]['atestation_month'], 
+      FACULTY_CFG[faculty]['atestation_day']))
+        year -= 1
+        return Date.civil(year, FACULTY_CFG[faculty]['atestation_month'], 
+        FACULTY_CFG[faculty]['atestation_day'])
+      end
+      return date
     end
-    return date
   end
 
   def is_actual?
@@ -39,8 +41,8 @@ class Attestation < Approval
   end
 
   # returns study plan
+  # for approval 
   def document
     study_plan
   end
-  
 end
---
+++ spec/models/atestation_spec.rb
@@ -0,0 +1,22 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+
+describe Index do
+  describe "Date computing" do
+    before :each do
+      Timecop.freeze(Time.zone.local(2010, 1, 2))
+      @faculty = Factory(:faculty)
+      FACULTY_CFG[@faculty.id] = {'atestation_month' => 3,
+                                  'atestation_day' => 15}
+    end
+
+    it "should compute date of next atestation" do
+      Atestation.next_for_faculty(@faculty).should == Date.civil(2010, 3, 15)
+    end
+
+    it "should compute date of actual atestation" do
+      Atestation.actual_for_faculty(@faculty).should == Date.civil(2009, 3, 15)     
+    end
+  end
+end
+
