8686f958e0360aad3a6b6bce836ad59ffe9b91b7
Josef Pospíšil <pepe@gravastar.cz>
2010-03-22 12:06:22 +0100

    Added start and end of study to absolved students report

--- lib/csv_exporter.rb
+++ lib/csv_exporter.rb
@@ -711,13 +711,14 @@ class CSVExporter
     # exports absolved students by faculties with study times
     def absolved_students_with_years
       is = Index.find_for(User.find_by_login('ticha'))
-      is = is.select {|i| i.absolved?}.sort {|i, j| i.year <=> j.year}
+      is = is.select {|i| i.absolved?}.sort {|i, j| i.semester <=> j.semester}
       isd = is.group_by(&:faculty)
       isd.each do |faculty|
         File.open("absolved_%s.csv" % faculty.first.short_name, 'wb') do |outfile|
           CSV::Writer.generate(outfile, ';') do |csv|
+            csv << ['student', 'semestr', 'start', 'konec']
             faculty.last.each do |index|
-              csv << [index.student.display_name, index.year]
+              csv << [index.student.display_name, index.semester, index.enrolled_on.to_date, index.disert_theme.defense_passed_on.to_date]
             end
           end
         end
