2c60a39792b8e56564e12e66b6b71053a2c782f9
Josef Pospíšil <josef.pospisil@laststar.eu>
2010-05-21 13:32:05 +0200

    Added migration for renaming approval types in db

--- db/migrate/20100519122554_change_approval_types.rb
+++ db/migrate/20100519122554_change_approval_types.rb
@@ -1,7 +1,13 @@
 class ChangeApprovalTypes < ActiveRecord::Migration
   def self.up
+    Approval.update_all("type='StudyPlanApproval'", "type='StudyPlanApprovement'")
+    Approval.update_all("type='DisertThemeApproval'", "type='DisertThemeApprovement'")
+    Approval.update_all("type='FinalExamApproval'", "type='FinalExamApprovement'")
+    Approval.update_all("type='ScholarshipApproval'", "type='ScholarshipApprovement'")
+    Approval.update_all("type='Attestation'", "type='Atestation'")
   end
 
   def self.down
+    raise ActiveRecord::IrreversibleMigration
   end
 end
