eadf85e416c029f8c8d72359450f0be3952f75ed
Josef Pospíšil <josef.pospisil@laststar.eu>
2010-07-07 14:12:18 +0200

    Removed unncessary view related method from study

--- app/models/study.rb
+++ app/models/study.rb
@@ -1,15 +1,4 @@
 class Study < ActiveRecord::Base
   
   has_many :canditats
-
-  # returns array structured for html select
-  def self.for_select(options = {})
-    result = find(:all).map {|s| [s.name, s.id]}
-    if options[:include_empty]
-      [['---', '0']].concat(result)
-    else
-      result
-    end
-  end
-
 end
