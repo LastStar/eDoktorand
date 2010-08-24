a154eb5d4076d3b2d207884727ab01ab9d011891
Josef Pospíšil <josef.pospisil@laststar.eu>
2010-08-24 15:25:22 +0200

    Changed deploy repo to github

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 deploy.application = "ruby.czu.cz"
-deploy.repository = 'ssh://git@gravastar.cz:2223/git/phdstudy'
+deploy.repository = 'git://github.com/pepe/eDoktorand.git'
 deploy.user = 'deploy'
 deploy.hosts = ['ruby.czu.cz']
 deploy.path = '/var/apps'
