6d6174433b516afb8425fac92fb2582f8949b5e2
Josef Pospíšil <josef.pospisil@laststar.eu>
2010-09-01 11:44:20 +0200

    Spec refactoring

--- spec/models/im_identity_spec.rb
+++ spec/models/im_identity_spec.rb
@@ -5,31 +5,33 @@ describe ImIndex do
     ImIdentity.new
   end
 
-  before(:each) do
-    @student = Factory(:student, :uic => 111222)
-    @identity = ImIdentity.create(:uic => @student.uic,
-                                  :loginname => 'student',
-                                  :status => 'N')
-  end
+  context "update user"do
+    before(:each) do
+      @student = Factory(:student, :uic => 111222)
+      @identity = ImIdentity.create(:uic => @student.uic,
+                                    :loginname => 'student',
+                                    :status => 'N')
+    end
 
-  it "has it's student" do
-    @identity.student.uic.should == @student.uic
-  end
+    it "has it's student" do
+      @identity.student.uic.should == @student.uic
+    end
 
-  it "creates user for its student" do
-    @identity.update_user
-    @student.user.login.should == 'student'
-  end
-  
-  it "updates existing student user" do
-    @student.user = Factory(:user, :person => @student)
-    @identity.update_user
-    @student.reload
-    @student.user.login.should == 'student'
-  end
+    it "creates user for its student" do
+      @identity.update_user
+      @student.user.login.should == 'student'
+    end
+    
+    it "updates existing student user" do
+      @student.user = Factory(:user, :person => @student)
+      @identity.update_user
+      @student.reload
+      @student.user.login.should == 'student'
+    end
 
-  it "set its status to S if it worked" do
-    @identity.update_user
-    @identity.status.should == 'S'
+    it "set its status to S if it worked" do
+      @identity.update_user
+      @identity.status.should == 'S'
+    end
   end
 end
