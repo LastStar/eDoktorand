c73ff16c2cc2933a59e952e34e77988d8234c62c
Josef Pospíšil <josef.pospisil@laststar.eu>
2010-09-01 11:22:54 +0200

    Added creating an updating of user from identity

--- app/models/im_identity.rb
+++ app/models/im_identity.rb
@@ -1,2 +1,13 @@
 class ImIdentity < ActiveRecord::Base
+
+  # returns its student by uic
+  def student
+    @student ||= Student.find_by_uic(uic)
+  end
+
+  # updates its student user
+  def update_user
+    @user = student.user || student.build_user
+    @user.update_attribute(:login, loginname)
+  end
 end
--- spec/models/im_identity_spec.rb
+++ spec/models/im_identity_spec.rb
@@ -4,4 +4,25 @@ describe ImIndex do
   it 'exists in system' do
     ImIdentity.new
   end
+
+  before(:each) do
+    @student = Factory(:student, :uic => 111222)
+    @identity = ImIdentity.create(:uic => @student.uic, :loginname => 'student')
+  end
+
+  it "has it's student" do
+    @identity.student.uic.should == @student.uic
+  end
+
+  it "creates user for its student" do
+    @identity.update_user
+    @student.user.login.should == 'student'
+  end
+  
+  it "updates existing student user" do
+    @student.user = Factory(:user, :person => @student)
+    @identity.update_user
+    @student.reload
+    @student.user.login.should == 'student'
+  end
 end
