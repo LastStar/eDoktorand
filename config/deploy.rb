0c21529b2add295ae579808fc89a95e3d9358db9
Josef Pospíšil <josef.pospisil@laststar.eu>
2010-09-02 15:18:54 +0200

    Changed deploy branch to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
-deploy.application = "ruby.czu.cz"
+deploy.application = "edoktorand.czu.cz"
 deploy.repository = 'git://github.com/pepe/eDoktorand.git'
 deploy.user = 'deploy'
 deploy.hosts = ['ruby.czu.cz']
 deploy.path = '/var/apps'
-deploy.branch = 'staging'
+deploy.branch = 'deploy'
 # deploy.ssh_opts = '-p 2222'
 
 # optional advanced settings
