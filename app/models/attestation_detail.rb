209009bfdf9be2699e181ef04a646b41a548cb40
Josef Pospíšil <josef.pospisil@laststar.eu>
2010-10-07 10:52:39 +0200

    Removed FIXME

--- app/models/attestation_detail.rb
+++ app/models/attestation_detail.rb
@@ -4,8 +4,6 @@ class AttestationDetail < ActiveRecord::Base
   validates_associated :study_plan
   
   def self.new_for(student)
-    #FIXME shitty fix with time translation to string. Maybe time zones in
-    #account
     return new(:study_plan_id => student.study_plan.id,
                :attestation_term => Attestation.next_for_faculty(student.faculty))
 
