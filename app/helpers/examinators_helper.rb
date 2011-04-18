6d7531d148852728789ba0d76777a681d6939899
Jan Křička <krica@gravastar.cz>
2011-04-18 11:38:36 +0200

    Examinators

--- app/helpers/examinators_helper.rb
+++ app/helpers/examinators_helper.rb
@@ -3,7 +3,7 @@ module ExaminatorsHelper
   def edit_link(examinator)
     link_to_remote(image_tag('change.png'),{
                    :url => {:action => 'edit', :id => examinator.department_employment.id},
-                   :update => "examinator_%i" % examinator.id},
+                   :update => dom_id(examinator)},
                    {:class => 'nobg'})
   end
   
