4b244909d8aa8fd61baea2e335bc1e9f0820cd51
Josef Pospíšil <josef.pospisil@laststar.eu>
2011-07-19 13:21:50 +0200

    Fix code design

--- app/models/get_subject_service.rb
+++ app/models/get_subject_service.rb
@@ -7,20 +7,20 @@ class GetSubjectService < Handsoap::Service
     # register namespaces for the request
     doc.alias 'tns', 'http://services'
   end
-  
+
   def on_response_document(doc)
     # register namespaces for the response
     doc.add_namespace 'ns', 'http://services'
   end
-  
+
   # public methods
-  
+
   def self.get_subjects
     soap_action = 'urn:getSubjects'
     response = invoke('tns:getSubjects', soap_action)
     response.xpath("//subject").map {|node| parse_subject(node)}
   end
-  
+
   private
   # helpers
 
