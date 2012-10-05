8dda73aa18b2c75c83d3f10dfb6359ce981f4aba
Josef Pospíšil <josef.pospisil@laststar.eu>
2012-10-05 13:24:00 +0200

    First spec prospect

--- spec/models/study_interrupt_spec.rb
+++ spec/models/study_interrupt_spec.rb
@@ -7,48 +7,65 @@ describe "Study Interrupt" do
     @index.save
   end
 
-  before :each do
-    Timecop.freeze(Time.zone.local(2010, 1, 2))
-    @interrupt = StudyInterrupt.create(:index => @index,
-                                 :start_on => Time.current,
-                                 :duration => 6)
-  end
-
-  describe "times and duration" do
-    it "should return end on computed from start" do
-      @interrupt.end_on.should == Time.zone.local(2010, 6, 30).end_of_month
-    end
-
-    it "should recompute start on to beginning of month before validation" do
-      @interrupt.start_on.should == Time.current.beginning_of_month
-    end
-
-    it "should compute current duration to time now if it not finished" do
-      Timecop.freeze(Time.zone.local(2010, 1, 2).end_of_month)
-      @interrupt.current_duration.should == 31.days.to_i
-    end
-
-    it "should compute current duration to finished on if finished" do
+  context "when duration in months" do
+    before :each do
       Timecop.freeze(Time.zone.local(2010, 1, 2))
-      @interrupt.finish!
-      Timecop.freeze(Time.zone.local(2010, 3, 2))
-      @interrupt.current_duration.should == 31.days.to_i
+      @interrupt = StudyInterrupt.create(:index => @index,
+                                   :start_on => Time.current,
+                                   :duration => 6)
     end
 
-    it "should return planned duration if not finished and and end on in past" do
-      Timecop.freeze(Time.zone.local(2010, 7, 2))
-      @interrupt.current_duration.should == 6.months.to_i
+    context "with times and duration" do
+      it "should return end on computed from start" do
+        @interrupt.end_on.should == Time.zone.local(2010, 6, 30).end_of_month
+      end
+
+      it "should recompute start on to beginning of month before validation" do
+        @interrupt.start_on.should == Time.current.beginning_of_month
+      end
+
+      it "should compute current duration to time now if it not finished" do
+        Timecop.freeze(Time.zone.local(2010, 1, 2).end_of_month)
+        @interrupt.current_duration.should == 31.days.to_i
+      end
+
+      it "should compute current duration to finished on if finished" do
+        Timecop.freeze(Time.zone.local(2010, 1, 2))
+        @interrupt.finish!
+        Timecop.freeze(Time.zone.local(2010, 3, 2))
+        @interrupt.current_duration.should == 31.days.to_i
+      end
+
+      it "should return planned duration if not finished and and end on in past" do
+        Timecop.freeze(Time.zone.local(2010, 7, 2))
+        @interrupt.current_duration.should == 6.months.to_i
+      end
+
     end
 
+    context "when changing states" do
+      it "should be finished" do
+        @interrupt.finished?.should be_false
+        @interrupt.finish!.should == true
+        @interrupt.finished?.should be_true
+        @interrupt.finished_on.should == Time.now.end_of_month
+      end
+    end
   end
 
-  describe "changing states" do
-    it "should be finished" do
-      @interrupt.finished?.should be_false
-      @interrupt.finish!.should == true
-      @interrupt.finished?.should be_true
-      @interrupt.finished_on.should == Time.now.end_of_month
+  describe "when duration in days" do
+    before :each do
+      Timecop.freeze(Time.zone.local(2012, 9, 20))
+      @interrupt = StudyInterrupt.create(:index => @index,
+                                   :start_on => Time.current,
+                                   :duration_in_days => true,
+                                   :duration => 6)
     end
+
+    it "should set duration to days" do
+      @interrupt.should be_valid
+    end
+
   end
 end
 
