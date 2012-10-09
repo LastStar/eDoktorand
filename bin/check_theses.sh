89238a046b184332f7321008144647a30ce41f56
Josef Pospíšil <josef.pospisil@laststar.eu>
2012-10-09 09:01:41 +0200

    Fix checker script

--- bin/check_theses.sh
+++ bin/check_theses.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cd /var/apps/edoktorand.czu.cz && ./script/runner -e production 'DisertThemes::Checker.send_for_check'
+cd /var/apps/edoktorand.czu.cz && ./script/runner -e production 'extend DisertThemes::Checker; send_for_check'
 
-cd /var/apps/edoktorand.czu.cz && ./script/runner -e production 'DisertThemes::Checker.receive_results'
+cd /var/apps/edoktorand.czu.cz && ./script/runner -e production 'extend DisertThemes::Checker; receive_results'
