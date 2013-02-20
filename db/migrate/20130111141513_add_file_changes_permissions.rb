c05af349620d0ee6a10d9f63116155dc2ab9bca6
Josef Pospíšil <josef.pospisil@laststar.eu>
2013-02-20 12:31:58 +0100

    Add stipendia's list for secretary
    Also some code design and migration fixes

--- db/migrate/20130111141513_add_file_changes_permissions.rb
+++ db/migrate/20130111141513_add_file_changes_permissions.rb
@@ -1,14 +1,15 @@
 class AddFileChangesPermissions < ActiveRecord::Migration
   def self.up
     %w(update_disert_theme update_self_report update_literature_review).each do |p|
-      puts p
-      Role.find(2).permissions << Permission.create(:name => "disert_themes/#(p)")
+      Role.find(2).permissions << Permission.create(:name => "disert_themes/#{p}")
     end
   end
 
   def self.down
     %w(update_disert_theme update_self_report update_literature_review).each do |p|
-      Permission.first(:conditions => {:name => "disert_themes/#(p)"}).destroy
+      p = Permission.first(:conditions => {:name => "disert_themes/#{p}"})
+      Role.find(2).permissions.delete p
+      p.destroy
     end
   end
 end
