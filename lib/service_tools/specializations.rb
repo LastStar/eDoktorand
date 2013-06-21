5a44ecb8d045244aa47c6639bcd753b064fdf775
Josef Pospíšil <josef.pospisil@laststar.eu>
2013-06-21 09:41:17 +0200

    Fix specialization fixing from central register

--- lib/service_tools/specializations.rb
+++ lib/service_tools/specializations.rb
@@ -2,11 +2,12 @@ module ServiceTools
   module Specializations
     # repairs all specializations from specializations service
     # based on response from CentralRegister::Specialization
-    def self.repair_all_by_code(response)
+    def self.repair_all_by_code(response, not_found = false)
       ss = Specialization.all
       banned = ["XEKOLO3", "XOCHL"].freeze
       response.each do |hash|
-        puts [hash[:msmtCode], hash[:language], hash[:shortName]].join(";")
+        hash[:msmtCode] = hash[:msmtCode].gsub(/x/, "V")
+        hash[:language] = "CZE" unless hash[:language].present?
         next if banned.include?(hash[:shortName])
         if (specializations = Specialization.all(:conditions => ["msmt_code = ? and locale = ?", hash[:msmtCode], hash[:language][0..-2].downcase] )).present?
           specializations.each do |specialization|
@@ -21,7 +22,7 @@ module ServiceTools
           puts hash.map {|k, v| v }.join(" ")
         end
       end
-      puts  ss.map {|s| [s.id, s.name, s.msmt_code,  s.code].join(";") }
+      puts  ss.map {|s| [s.id, s.name, s.msmt_code,  s.code].join(";") } if not_found
     end
   end
 end
